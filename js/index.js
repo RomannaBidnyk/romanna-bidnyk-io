944a86e7666eb504495d83a79eb21b362e18ef13
RomannaBidnyk <romanna.bidnyk@gmail.com>
2024-08-28 20:47:24 -0400

    created index.js and added footer

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const footer = document.createElement('footer');
+
+footer.textContent = '© 2024 Romanna Bidnyk';
+
+footer.style.backgroundColor = '#d8dbfe';
+footer.style.padding = '10px';
+footer.style.textAlign = 'center';
+
+document.body.appendChild(footer);
