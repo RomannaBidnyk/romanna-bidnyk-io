5a1859de21b034cdc772e6e174906d9c77717e55
RomannaBidnyk <romanna.bidnyk@gmail.com>
2024-08-29 22:17:09 -0400

    added copyright text in footer

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
+// Add Footer
 const footer = document.createElement('footer');
-
-footer.textContent = '© 2024 Romanna Bidnyk';
 
 footer.style.backgroundColor = '#d8dbfe';
 footer.style.padding = '10px';
 footer.style.textAlign = 'center';
 
 document.body.appendChild(footer);
+
+//Copyright Text in Footer
+let today = new Date();
+let thisYear = today.getFullYear();
+
+const copyright = document.createElement('p');
+copyright.innerHTML = `Romanna Bidnyk &#169; ${thisYear}`;
+
+footer.appendChild(copyright);
